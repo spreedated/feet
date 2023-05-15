3fa2727057621a249cb574626e57f1afb8fa67f8
SpReeDateD <webmaster@born2skate.de>
2023-05-15 23:59:43 +0200

    added strong name

--- FeetScraper/AssemblyInfo.cs
+++ FeetScraper/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
 
 [assembly: Guid("57fd2fb1-ec66-46d1-b67f-b820fb17355f")]
-[assembly: InternalsVisibleTo("FeetScraperTests")]
+[assembly: InternalsVisibleTo("FeetScraperTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a12f3e4a619c3a095a14f73244fe9a9037eddec62d34c901a881e14fd0bcace0804d81b798aa7c62741d78c4fd7795a14efcb4ee895d808863f6da911776a44ac2ee3e83bd0fbed3f5cb9c0b288af2180dc29bdf61f63cce23c0c1e178f8cb3a04c0958ffe56ae07a97a250a26c79cced3cfecb38e4a4f64e1bd78628a148cc3")]

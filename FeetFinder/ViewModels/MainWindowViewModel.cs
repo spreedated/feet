63cedb2dc38192035ba070f6fbd65a53da84abdf
SpReeDateD <webmaster@born2skate.de>
2023-05-18 20:50:04 +0200

    updates to library

---
+++ FeetFinder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using neXn.Lib.Wpf.ViewLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeetFinder.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
